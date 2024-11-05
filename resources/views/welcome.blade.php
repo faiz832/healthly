60b7d7713d86767a9de2d71cb317473eee6ffe0e
Faiz <faqodefazz@gmail.com>
2024-11-05 17:58:37 +0700

    asd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,12 +16,318 @@
         rel="stylesheet">
 
     <!-- Styles -->
-    @vite(['resources/css/app.css', 'resources/css/carousel.css', 'resources/js/app.js', 'resources/js/carousel.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         /* Hide scrollbar in Chrome, Safari and Opera */
         ::-webkit-scrollbar {
             display: none;
+        }
+
+        /* carousel */
+        :root {
+            --item1-transform: translateX(-100%) translateY(-5%) scale(1.5);
+            --item1-filter: blur(30px);
+            --item1-zIndex: 11;
+            --item1-opacity: 0;
+
+            --item2-transform: translateX(0);
+            --item2-filter: blur(0px);
+            --item2-zIndex: 10;
+            --item2-opacity: 1;
+
+            --item3-transform: translate(50%, 10%) scale(0.8);
+            --item3-filter: blur(10px);
+            --item3-zIndex: 9;
+            --item3-opacity: 1;
+
+            --item4-transform: translate(90%, 20%) scale(0.5);
+            --item4-filter: blur(30px);
+            --item4-zIndex: 8;
+            --item4-opacity: 1;
+
+            --item5-transform: translate(120%, 30%) scale(0.3);
+            --item5-filter: blur(40px);
+            --item5-zIndex: 7;
+            --item5-opacity: 0;
+        }
+
+        /* carousel */
+        .carousel {
+            position: relative;
+            height: 600px;
+        }
+
+        .carousel .list {
+            position: absolute;
+            width: 1140px;
+            max-width: 90%;
+            height: 80%;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+
+        .carousel .list .item {
+            position: absolute;
+            left: 0%;
+            width: 70%;
+            height: 100%;
+            font-size: 15px;
+            transition: left 0.5s, opacity 0.5s, width 0.5s;
+        }
+
+        .carousel .list .item:nth-child(n + 6) {
+            opacity: 0;
+        }
+
+        .carousel .list .item:nth-child(2) {
+            z-index: 10;
+            transform: translateX(0);
+        }
+
+        .carousel .list .item img {
+            width: 50%;
+            position: absolute;
+            right: 0;
+            top: 50%;
+            transform: translateY(-50%);
+            transition: right 1.5s;
+        }
+
+        .carousel .list .item .introduce {
+            opacity: 0;
+            pointer-events: none;
+        }
+
+        .carousel .list .item:nth-child(2) .introduce {
+            opacity: 1;
+            pointer-events: auto;
+            width: 400px;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            transition: opacity 0.5s;
+        }
+
+        .carousel .list .item .introduce .title {
+            font-size: 2em;
+            font-weight: 500;
+            line-height: 1em;
+        }
+
+        .carousel .list .item .introduce .topic {
+            font-size: 4em;
+            font-weight: 500;
+        }
+
+        .carousel .list .item .introduce .des {
+            font-size: small;
+            color: #5559;
+        }
+
+        .carousel .list .item:nth-child(1) {
+            transform: var(--item1-transform);
+            filter: var(--item1-filter);
+            z-index: var(--item1-zIndex);
+            opacity: var(--item1-opacity);
+            pointer-events: none;
+        }
+
+        .carousel .list .item:nth-child(3) {
+            transform: var(--item3-transform);
+            filter: var(--item3-filter);
+            z-index: var(--item3-zIndex);
+        }
+
+        .carousel .list .item:nth-child(4) {
+            transform: var(--item4-transform);
+            filter: var(--item4-filter);
+            z-index: var(--item4-zIndex);
+        }
+
+        .carousel .list .item:nth-child(5) {
+            transform: var(--item5-transform);
+            filter: var(--item5-filter);
+            opacity: var(--item5-opacity);
+            pointer-events: none;
+        }
+
+        /* animation text in item2 */
+        .carousel .list .item:nth-child(2) .introduce .title,
+        .carousel .list .item:nth-child(2) .introduce .topic,
+        .carousel .list .item:nth-child(2) .introduce .des {
+            opacity: 0;
+            animation: showContent 0.5s 1s ease-in-out 1 forwards;
+        }
+
+        @keyframes showContent {
+            from {
+                transform: translateY(-30px);
+                filter: blur(10px);
+            }
+
+            to {
+                transform: translateY(0);
+                opacity: 1;
+                filter: blur(0px);
+            }
+        }
+
+        .carousel .list .item:nth-child(2) .introduce .topic {
+            animation-delay: 1.2s;
+        }
+
+        .carousel .list .item:nth-child(2) .introduce .des {
+            animation-delay: 1.4s;
+        }
+
+        /* next click */
+        .carousel.next .item:nth-child(1) {
+            animation: transformFromPosition2 0.5s ease-in-out 1 forwards;
+        }
+
+        @keyframes transformFromPosition2 {
+            from {
+                transform: var(--item2-transform);
+                filter: var(--item2-filter);
+                opacity: var(--item2-opacity);
+            }
+        }
+
+        .carousel.next .item:nth-child(2) {
+            animation: transformFromPosition3 0.7s ease-in-out 1 forwards;
+        }
+
+        @keyframes transformFromPosition3 {
+            from {
+                transform: var(--item3-transform);
+                filter: var(--item3-filter);
+                opacity: var(--item3-opacity);
+            }
+        }
+
+        .carousel.next .item:nth-child(3) {
+            animation: transformFromPosition4 0.9s ease-in-out 1 forwards;
+        }
+
+        @keyframes transformFromPosition4 {
+            from {
+                transform: var(--item4-transform);
+                filter: var(--item4-filter);
+                opacity: var(--item4-opacity);
+            }
+        }
+
+        .carousel.next .item:nth-child(4) {
+            animation: transformFromPosition5 1.1s ease-in-out 1 forwards;
+        }
+
+        @keyframes transformFromPosition5 {
+            from {
+                transform: var(--item5-transform);
+                filter: var(--item5-filter);
+                opacity: var(--item5-opacity);
+            }
+        }
+
+        /* previous */
+        .carousel.prev .list .item:nth-child(5) {
+            animation: transformFromPosition4 0.5s ease-in-out 1 forwards;
+        }
+
+        .carousel.prev .list .item:nth-child(4) {
+            animation: transformFromPosition3 0.7s ease-in-out 1 forwards;
+        }
+
+        .carousel.prev .list .item:nth-child(3) {
+            animation: transformFromPosition2 0.9s ease-in-out 1 forwards;
+        }
+
+        .carousel.prev .list .item:nth-child(2) {
+            animation: transformFromPosition1 1.1s ease-in-out 1 forwards;
+        }
+
+        @keyframes transformFromPosition1 {
+            from {
+                transform: var(--item1-transform);
+                filter: var(--item1-filter);
+                opacity: var(--item1-opacity);
+            }
+        }
+
+        /* detail  */
+        .carousel .list .item .detail {
+            opacity: 0;
+            pointer-events: none;
+        }
+
+        .arrows {
+            position: absolute;
+            width: 1140px;
+            max-width: 90%;
+            display: flex;
+            justify-content: space-between;
+            left: 50%;
+            transform: translateX(-50%);
+        }
+
+        #prev,
+        #next {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            font-family: monospace;
+            border: 1px solid #5555;
+            font-size: large;
+            bottom: 20%;
+            left: 10%;
+        }
+
+        #next {
+            left: unset;
+            right: 10%;
+        }
+
+        #back {
+            position: absolute;
+            z-index: 100;
+            bottom: 0%;
+            left: 50%;
+            transform: translateX(-50%);
+            border: none;
+            border-bottom: 1px solid #555;
+            font-family: Poppins;
+            font-weight: bold;
+            letter-spacing: 3px;
+            background-color: transparent;
+            padding: 10px;
+            /* opacity: 0; */
+            transition: opacity 0.5s;
+        }
+
+        @media screen and (max-width: 767px) {
+
+            /* mobile */
+            .carousel {
+                height: 600px;
+            }
+
+            .carousel .list .item {
+                width: 100%;
+                font-size: 10px;
+            }
+
+            .carousel .list {
+                height: 100%;
+            }
+
+            .carousel .list .item:nth-child(2) .introduce {
+                width: 50%;
+            }
+
+            .carousel .list .item img {
+                width: 40%;
+            }
         }
 
         /* Swing 1: Berlawanan arah jarum jam */
@@ -725,6 +1031,39 @@
                 }
             });
         });
+
+        //carousel animation
+        let nextButton = document.getElementById("next");
+        let prevButton = document.getElementById("prev");
+        let carousel = document.querySelector(".carousel");
+        let listHTML = document.querySelector(".carousel .list");
+
+        nextButton.onclick = function() {
+            showSlider("next");
+        };
+        prevButton.onclick = function() {
+            showSlider("prev");
+        };
+        let unAcceppClick;
+        const showSlider = (type) => {
+            nextButton.style.pointerEvents = "none";
+            prevButton.style.pointerEvents = "none";
+
+            carousel.classList.remove("next", "prev");
+            let items = document.querySelectorAll(".carousel .list .item");
+            if (type === "next") {
+                listHTML.appendChild(items[0]);
+                carousel.classList.add("next");
+            } else {
+                listHTML.prepend(items[items.length - 1]);
+                carousel.classList.add("prev");
+            }
+            clearTimeout(unAcceppClick);
+            unAcceppClick = setTimeout(() => {
+                nextButton.style.pointerEvents = "auto";
+                prevButton.style.pointerEvents = "auto";
+            }, 2000);
+        };
 
         // Function to scroll to top
         function scrollToTop() {
