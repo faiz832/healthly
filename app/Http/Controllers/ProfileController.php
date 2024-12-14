4ee8ac94ec0ea85dac081da410a76713d4415055
faiz832 <faqodefazz@gmail.com>
2024-12-14 19:05:46 +0700

    asd

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,9 +12,6 @@ use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
     public function edit(Request $request): View
     {
         return view('profile.edit', [
